44885c8007ddf168b432baa5dc00c5a95d8ec529
riteshnath <nathritesh98@gmail.com>
2023-10-14 15:46:31 +0530

    email for password reset

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,16 +1,18 @@
-const nodeMailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  const transporter = nodeMailer.createTransport({
-    service: process.env.SMTP_SERVICE,
+  // 1) Create a Transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: process.env.SMTP_MAIL,
+    from: "Ritesh Nath <ritesh@official.io>",
     to: options.email,
     subject: options.subject,
     text: options.message,
